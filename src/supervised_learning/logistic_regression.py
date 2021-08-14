a9c05ae7863c0159cdc5ad1e7fd23d26f7ded1be
Danial Khosravi <dani_khosravi@yahoo.com>
2021-08-14 14:08:33 +1000

    feat: added multinomial and ovr multi class logistic classification implementations

--- src/supervised_learning/logistic_regression.py
+++ src/supervised_learning/logistic_regression.py
@@ -33,10 +33,11 @@ class LogisticRegression(BaseEstimator):
         return self._sigmoid(X @ self._weights + self._bias)
 
     def predict(self, X):
-        y_pred = self.predict_proba(X)
-        return np.round(y_pred)
+        y_prop = self.predict_proba(X)
+        return np.round(y_prop)
 
-    def _sigmoid(self, x):
+    @staticmethod
+    def _sigmoid(x):
         return 1 / (1 + np.exp(-x))
 
 
@@ -66,9 +67,89 @@ class LogisticRegressionV2(BaseEstimator):
             elif self.regularization == 'l2':
                 self._theta -= self.learning_rate * (self.C * (A.T @ error) + np.sum(self._theta)) / n_samples
 
-    def predict(self, X):
-        y_pred = self._sigmoid(X.dot(self._theta[1:]) + self._theta[0])
-        return np.round(y_pred)
+    def predict_proba(self, X):
+        return self._sigmoid(X.dot(self._theta[1:]) + self._theta[0])
 
-    def _sigmoid(self, x):
+    def predict(self, X):
+        y_prop = self.predict_proba(X)
+        return np.round(y_prop)
+
+    @staticmethod
+    def _sigmoid(x):
         return 1 / (1 + np.exp(-x))
+
+
+class MultinomialLogisticRegression(BaseEstimator):
+    def __init__(self, learning_rate=0.1, n_iterations=4000, regularization=None, C=0.1):
+        self.learning_rate = learning_rate
+        self.n_iterations = n_iterations
+        self.regularization = regularization
+        self.C = C
+
+    def _init_parameters(self, X, y):
+        n_features = X.shape[1]
+        n_labels = y.shape[1]
+        self._theta = np.zeros((n_features + 1, n_labels))
+
+    def fit(self, X, y):
+        n_samples = X.shape[0]
+
+        self._init_parameters(X, y)
+
+        A = np.concatenate([np.ones((n_samples, 1)), X], axis=1)
+
+        for i in range(self.n_iterations):
+            y_pred = self._softmax(A @ self._theta)
+            error = y_pred - y
+            if self.regularization is None:
+                self._theta -= self.learning_rate * (A.T @ error) / n_samples
+            elif self.regularization == 'l2':
+                self._theta -= self.learning_rate * (self.C * (A.T @ error) + np.sum(self._theta, axis=0)) / n_samples
+
+    def predict(self, X):
+        n_samples = X.shape[0]
+        A = np.concatenate([np.ones((n_samples, 1)), X], axis=1)
+        return self._softmax(A @ self._theta)
+
+    @staticmethod
+    def _softmax(x):
+        return np.exp(x) / np.exp(x).sum(axis=0)
+
+
+class OneVsRestLogisticRegression(BaseEstimator):
+    def __init__(self, learning_rate=0.1, n_iterations=4000, regularization=None, C=0.1):
+        self.learning_rate = learning_rate
+        self.n_iterations = n_iterations
+        self.regularization = regularization
+        self.C = C
+
+        self._models = []
+
+    def fit(self, X, y):
+        n_labels = y.shape[1]
+
+        for label in range(n_labels):
+            ovr_y = y.argmax(axis=1)
+            idx = ovr_y == label
+            ovr_y[idx] = 1
+            ovr_y[np.invert(idx)] = 0
+            model = LogisticRegression(
+                learning_rate=self.learning_rate,
+                n_iterations=self.n_iterations,
+                regularization=self.regularization,
+                C=self.C
+            )
+            model.fit(X, ovr_y)
+            self._models.append(model)
+
+    def predict(self, X):
+        n_labels = len(self._models)
+        n_samples = X.shape[0]
+
+        y_pred = np.zeros((n_samples, n_labels))
+
+        for i, model in enumerate(self._models):
+            prob = model.predict_proba(X)
+            y_pred[:, i] = prob
+
+        return y_pred
